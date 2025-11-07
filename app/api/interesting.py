f39a8ffe20e6ec954fb797ad5114d24c7bd9e614
Rasmus Thornberg <rasmus.thornberg@gmail.com>
2025-11-07 23:19:15 +0100

    compat: re-export list_interesting/interesting_summary for legacy imports

--- app/api/interesting.py
+++ app/api/interesting.py
@@ -4,4 +4,5 @@ from app.api.routers.interesting import (
     list_interesting as list_interesting,
     interesting_summary as interesting_summary,
 )
+
 __all__ = ["router", "list_interesting", "interesting_summary"]
