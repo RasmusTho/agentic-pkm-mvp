34f2550a87d7941b46ed00d99e929fb63fab8db8
Rasmus Thornberg <rasmus.thornberg@gmail.com>
2025-11-19 21:19:22 +0100

    Add CLI panel-update command

--- app/agents/panel/__init__.py
+++ app/agents/panel/__init__.py
@@ -2,6 +2,7 @@
 
 from .agent import PanelAgentResult, handle_note_update
 from .events import panel_intent_to_event
+from .integration import PanelPipelineResult, handle_panel_update, panel_events_enabled
 from .intents import PanelIntent, diff_panel_states, enrich_panel_intents, resolve_action_event_type
 from .parser import parse_panel
 from .schema import PanelAction, PanelLogEntry, PanelState
@@ -12,10 +13,13 @@ __all__ = [
     "PanelState",
     "PanelIntent",
     "PanelAgentResult",
+    "PanelPipelineResult",
     "parse_panel",
     "diff_panel_states",
     "enrich_panel_intents",
     "resolve_action_event_type",
     "panel_intent_to_event",
     "handle_note_update",
+    "handle_panel_update",
+    "panel_events_enabled",
 ]
