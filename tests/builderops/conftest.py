03dc7a1863b415d1d8c3c8463506b4b9288aee9e
Rasmus Thornberg <rasmus.thornberg@gmail.com>
2026-09-16 11:24:18 +0200

    Fix shared PG fixture registration

--- tests/builderops/conftest.py
+++ tests/builderops/conftest.py
@@ -1,5 +1,6 @@
 """BuilderOps-wide test fixture registration."""
 
 from tests.builderops.issue_delivery_production_harness import (
+    issue_delivery_pg_store as issue_delivery_pg_store,
     issue_delivery_production_harness as issue_delivery_production_harness,
 )
